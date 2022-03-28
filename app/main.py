f66bdfbf5d43d2d211a9a4dcb84ca5b651c29254
pieas-asif <pieas.asif4@gmail.com>
2022-03-28 23:10:41 +0600

    get and post

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-from random import randrange
 from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
 import psycopg
@@ -20,18 +19,6 @@ class Post(BaseModel):
     published: bool = True
 
 
-posts = [
-    {"title": "Initial Post", "content": "Post Content", "id": 1},
-    {"title": "Initial Post Two", "content": "Post Content", "id": 2}
-]
-
-
-def get_a_post(id: int):
-    for post in posts:
-        if(post["id"] == id):
-            return post
-
-
 @app.get('/')
 def health_check():
     # health check api
@@ -41,24 +28,23 @@ def health_check():
 @app.get('/post')
 def all_posts():
     posts = conn.execute("""SELECT * FROM posts""").fetchall()
-    print(posts)
     return {"posts": posts}
 
 
 @app.get('/post/{id}', status_code=status.HTTP_302_FOUND)
 def get_post(id: int, response: Response):
     # get a post by id
-    post = get_a_post(id)
+    post = conn.execute(
+        """SELECT * FROM posts WHERE id = %s""", (str(id),)).fetchone()
     if not post:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {"data": f"No Post Found For Id {id}", }
     return {"data": post}
 
 
-@app.post('/post')
+@app.post('/post', status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
-    # create a post
-    post_dict = post.dict()
-    post_dict["id"] = randrange(0, 1000000)
-    posts.append(post_dict)
-    return post.dict()
+    post = conn.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
+                        (post.title, post.content, post.published,)).fetchone()
+    conn.commit()
+    return {"data": post}
