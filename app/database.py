3ff9d596dd9f479865db7b5da552d10faa927d83
pieas-asif <pieas.asif4@gmail.com>
2022-03-29 22:36:57 +0600

    database connection with orm

---
+++ app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://shaman:<password>@localhost/fastapi_course_db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+base = declarative_base()
